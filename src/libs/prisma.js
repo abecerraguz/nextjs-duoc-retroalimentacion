1fcfc9da14941448a5e47374c641cfa710ce583c
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2026-04-20 08:53:50 -0400

    fix: use pg.Pool instance in PrismaPg adapter (fixes production DB connection)

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,13 +1,16 @@
 import { PrismaClient } from "../generated/prisma/client/client"
 import { PrismaPg } from "@prisma/adapter-pg"
+import pg from "pg"
 
 const globalForPrisma = globalThis
 
-const createAdapter = () =>
-  new PrismaPg({
+const createAdapter = () => {
+  const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
   })
+  return new PrismaPg(pool)
+}
 
 export const prisma =
   globalForPrisma.prisma ?? new PrismaClient({ adapter: createAdapter() })
